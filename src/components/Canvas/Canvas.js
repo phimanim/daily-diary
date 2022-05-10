13ebf77a2448a0fbca09e03ce465b12cee507eb4
phimanim <aliker.imane@gmail.com>
2022-05-10 22:54:59 +0900

    can finally drag elements without canvas with react-draggable package

--- src/components/Canvas/Canvas.js
+++ src/components/Canvas/Canvas.js
@@ -1,4 +1,5 @@
 import React, { useRef, useEffect, useState } from "react";
+import ReactDOM from 'react-dom';
 
 function CanvasFrame() {
   const canvas = useRef();
@@ -10,7 +11,7 @@ function CanvasFrame() {
   const [text, setText] = useState({
     message: "",
     x: 20,
-    y: ""
+    y: "",
   });
   console.log("text state", text);
 
@@ -22,23 +23,23 @@ function CanvasFrame() {
     drawText();
   }, [text.message]);
 
-  function handleChange({target}) {
+  function handleChange({ target }) {
     const { value } = target;
     setText({
       ...text,
       message: value,
-      y: messages.length * 20 + 20
-  })
-  };
+      y: messages.length * 35 + 55,
+    });
+  }
 
   const handleClick = async (event) => {
     event.preventDefault();
-    setMessages(messages => messages.concat(text))
+    setMessages((messages) => messages.concat(text));
     console.log(messages);
-  }
+  };
 
   function drawText() {
-    ctx.font = "35px";
+    ctx.font = "35px Arial";
     ctx.fillStyle = "red";
     ctx.textAlign = "start";
     ctx.clearRect(50, 110, 500, -50);
@@ -47,10 +48,18 @@ function CanvasFrame() {
       let text = messages[i];
       ctx.fillText(text.message, text.x, text.y);
     }
-  }
 
+    let currentX = 600/2;
+    let currentY = 600/2;
+    let filling = "bonjour";
+    let widthFill = ctx.measureText(filling);
+console.log(widthFill)
+    console.log("width", widthFill)
+    ctx.fillText("bonjour", currentX - widthFill.width/2 , currentY);
+  }
   return (
     <div className="Canvas">
+      
       <canvas ref={canvas}></canvas>
       <form>
         <input
--- src/components/Canvas/useCanvas.js
+++ src/components/Canvas/useCanvas.js
@@ -1,23 +1,94 @@
-import React, { useRef, useEffect, useState } from "react";
+import React from "react";
+import ReactDOM from "react-dom";
+import Draggable from "react-draggable";
+import './Canvas.css';
+function UseCanvas() {
 
-function useCanvas() {
-  const [x, setX] = useState();
-  const [y, setY] = useState();
+    const handleTextInput = async (event) => {
+        event.preventDefault();
+        let daily = React.createElement('h1', {}, 'My First React Code');
+        ReactDOM.render(
+            daily,
+            document.getElementById('redbox')
+          );
+      };
 
-  useEffect(() => {
-    const update = (e) => {
-      setX(e.x);
-      setY(e.y);
-    };
-    window.addEventListener("mousemove", update);
-    window.addEventListener("touchmove", update);
+  return (
+      <div>
+<div
+      className="box"
+      id="redbox"
+      style={{
+        height: "500px",
+        width: "500px",
+        position: "relative",
+        overflow: "hidden",
+        padding: "0",
+        backgroundColor: "red",
+      }}
+    >
+      <Draggable bounds="parent">
+        <div
+          className="box"
+          style={{
+            height: "40px",
+            width: "40px",
+            overflow: "hidden",
+            padding: "0",
+            backgroundColor: "green",
+          }}
+        >
+          I can only be moved within my offsetParent.
+          <br />
+          <br />
+          Both parent padding and child margin work properly.
+        </div>
+      </Draggable>
+      <Draggable bounds="parent">
+        <div
+          className="box"
+          style={{
+            height: "40px",
+            width: "40px",
+            overflow: "hidden",
+            padding: "0",
+            backgroundColor: "blue",
+          }}
+        >
+          I also can only be moved within my offsetParent.
+          <br />
+          <br />
+          Both parent padding and child margin work properly.
+        </div>
+      </Draggable>
+      <Draggable bounds="parent">
+        <input className="DailyText" type="text" name="message" placeholder="Text" style={{background:"transparent", overflow:"auto", border:"none", }}/>
+      </Draggable>
+    </div>
+    <button onClick={handleTextInput}>
+        Text
+    </button>
+      </div>
     
-    return () => {
-      window.removeEventListener("mousemove", update);
-      window.removeEventListener("touchmove", update);
-    };
-  }, [setX, setY]);
-  return x && y ? <h1>{`x: ${x}; y: ${y};`}</h1> : null;
+  );
 }
 
-export default useCanvas;
+{
+  /* <Draggable
+        axis="x"
+        handle=".handle"
+        defaultPosition={{x: 0, y: 0}}
+        position={null}
+        grid={[25, 25]}
+        scale={1}
+        onStart={this.handleStart}
+        onDrag={this.handleDrag}
+        onStop={this.handleStop}>
+        <div>
+          <div className="handle">Drag from here</div>
+          <div>This readme is really dragging on...</div>
+        </div>
+      </Draggable> */
+}
+
+export default UseCanvas;
